ba99f2725b70563ba955d04d6876ecd35bb0f710
Maxime Brunet <maximb84@gmail.com>
2023-09-05 17:16:11 +0200

    Edition fichier script.js modale contact

--- functions.php
+++ functions.php
@@ -10,6 +10,14 @@ function motaphoto_enqueue_styles() {
 
 add_action( 'wp_enqueue_scripts', 'motaphoto_enqueue_styles' );
 
+
+function enqueue_my_scripts() {
+  wp_enqueue_script( 'my-script', get_template_directory_uri() . '/js/my-script.js', array('jquery'), '1.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_my_scripts' );
+
+
 function register_my_menus() {
     register_nav_menus(
       array(
---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contactButton = document.querySelector("#contactModal");
+    
+    if (contactButton) {
+      contactButton.addEventListener("click", function() {
+        // Votre code pour ouvrir la modale ici
+      });
+    }
+  });
+  
