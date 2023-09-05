db456e137cbf278b750f99e5787a86193a99458a
Maxime Brunet <maximb84@gmail.com>
2023-09-05 12:48:27 +0200

    Création du header

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function motaphoto_enqueue_styles() {
     // Charger le fichier CSS principal de WordPress (style.css)
     wp_enqueue_style( 'main-stylesheet', get_stylesheet_uri() );
     
-    // Charger votre fichier CSS personnalisé (styles.css)
+    // Charger le fichier CSS personnalisé (styles.css)
     wp_enqueue_style( 'custom-stylesheet', get_template_directory_uri() . '/css/styles.css' );
 }
 
---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Motaphoto</title>
+    <?php wp_head(); ?>
+</head>
+<header>
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo.png" alt="Logo du site">
+</header>
+<body>
+    
