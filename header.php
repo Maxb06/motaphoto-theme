44db5a499e27346add5f21940f48a8e431f8985b
Maxime Brunet <maximb84@gmail.com>
2023-09-05 15:20:51 +0200

    Mise en place de SASS

--- header.php
+++ header.php
@@ -6,11 +6,17 @@
     <title>Motaphoto</title>
     <?php wp_head(); ?>
 </head>
-<header>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo.png" alt="Logo du site">
-    <nav>
-        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-    </nav>
-</header>
 <body>
+    <div class="container">
+        <header>
+            <div class="header-menu">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo.png" alt="Logo du site">
+                <nav>
+                    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+                </nav>
+            </div>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/nathalie-11.jpeg" alt="Image présentation site">
+        </header>
+        <main>
+
     
