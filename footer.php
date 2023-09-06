4878193a83157dc640d16823bd0cab6bc98964eb
Maxime Brunet <maximb84@gmail.com>
2023-09-06 15:25:14 +0200

    Modification du nom de dossier template-parts et modif path

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
                 <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
                 <p>TOUS DROITS RÉSERVÉS</p>
             </div>
-            <?php get_template_part('template_parts/contact-modal'); ?>
+            <?php get_template_part('template-parts/contact-modal'); ?>
         </footer>
     </div> <!-- #page -->
     <?php wp_footer(); ?>
