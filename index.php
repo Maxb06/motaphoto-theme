10e23815f1cb2c15f898f0c27d472a9930423f06
Maxime Brunet <maximb84@gmail.com>
2023-09-08 15:20:12 +0200

    Ajout boucle WordPres de base index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php get_header(); ?>
 
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+<?php endwhile; else : ?>
+    <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
+
+
 <?php get_footer(); ?>
