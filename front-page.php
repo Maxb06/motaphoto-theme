dbbd0a720e1dbbe2cc095f8107971b5047807179
Maxime Brunet <maximb84@gmail.com>
2023-09-08 15:37:27 +0200

    Ajout photo hero header

--- front-page.php
+++ front-page.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
 
-
+        <section>
+            <div class="hero-header">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/nathalie-11.jpeg" alt="Grande photo en-tête">
+            </div>
+        </section>
 
 <?php get_footer(); ?>
