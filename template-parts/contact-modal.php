84c37002d103df303a7864910fcb2bffda0578ea
Maxime Brunet <maximb84@gmail.com>
2023-09-11 12:21:48 +0200

    Modification shortcode modale

--- template-parts/contact-modal.php
+++ template-parts/contact-modal.php
@@ -1,5 +1,5 @@
 <div id="contactModal" class="modal">
     <div class="modal-content">
-        <?php echo do_shortcode('[contact-form-7 id="b047b02" title="Modale Contact"]'); ?>
+        <?php echo do_shortcode('[contact-form-7 id="05e20ed" title="Formulaire de contact 1"]'); ?>
     </div>
 </div> 
