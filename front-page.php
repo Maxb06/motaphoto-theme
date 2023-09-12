e124e5f48dad4c53fab31b5510584c5e2a7ff562
Maxime Brunet <maximb84@gmail.com>
2023-09-12 16:39:49 +0200

    Ajout structure et code page d'accueil

--- front-page.php
+++ front-page.php
@@ -6,4 +6,36 @@
             </div>
         </section>
 
+        <div class="photo-catalog">
+            <h1>Catalogue de photos</h1>
+            <?php
+                $args = array(
+                    'post_type' => 'photographie',
+                    'posts_per_page' => 8, 
+                );
+                $the_query = new WP_Query( $args );
+            ?>
+
+            <?php if ( $the_query->have_posts() ) : ?>
+
+            <!-- Boucle requête perso -->
+            <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                <div class="photo-item">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php if ( has_post_thumbnail() ) : ?>
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                         <?php endif; ?>
+                        <h2><?php the_title(); ?></h2>
+                    </a>
+                </div>
+
+                <?php endwhile; ?> <!-- fin de la boucle -->
+                <?php wp_reset_postdata(); ?>
+
+            <?php else : ?> 
+                <p><?php _e( 'Désolé, aucune photo n\'a été trouvée.' ); ?></p>
+            <?php endif; ?>
+        </div>
+
+
 <?php get_footer(); ?>
