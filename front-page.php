8202654413954d5a53e0574e949a3b333fa44b4f
Maxime Brunet <maximb84@gmail.com>
2023-09-12 18:15:30 +0200

    Modif taille vignetet photo

--- front-page.php
+++ front-page.php
@@ -23,7 +23,7 @@
                 <div class="photo-item">
                     <a href="<?php the_permalink(); ?>">
                         <?php if ( has_post_thumbnail() ) : ?>
-                            <?php the_post_thumbnail('thumbnail'); ?>
+                            <?php the_post_thumbnail('full'); ?>
                          <?php endif; ?>                    
                     </a>
                 </div>
