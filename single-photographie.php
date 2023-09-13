c07e6321898d066172cd6472e386348bd8c0b61f
Maxime Brunet <maximb84@gmail.com>
2023-09-13 11:38:48 +0200

    Màj page photogaphie

--- single-photographie.php
+++ single-photographie.php
@@ -11,25 +11,25 @@
  
  /* Start the Loop */
  while ( have_posts() ) :
-	 the_post();
-	 ?>
-	 <div class="container">
-		 <div class="left-column">
-		 <h2><?php the_title(); ?></h2>
+	the_post();
+	?>
+	<div class="container">
+		<div class="left-column photo-info">
+		 	<h2><?php the_title(); ?></h2>
 			<p><strong>Référence :</strong> <?php the_field('reference'); ?></p>
 			<p><strong>Catégorie :</strong> <?php the_terms( $post->ID, 'categorie-photo', '', ', ', '' ); ?></p>
 			<p><strong>Format :</strong> <?php the_terms( $post->ID, 'format-photo', '', ', ', '' ); ?></p>
 			<p><strong>Type :</strong> <?php the_field('type'); ?></p>
 			<p><strong>Date :</strong> <?php the_date('Y'); ?></p>
-		 </div>
-		 <div class="right-column">
+		</div>
+		<div class="right-column photo-display">
 			<?php 
 			if ( has_post_thumbnail() ) {
 			the_post_thumbnail('full');
 			} 
 			?>
-		 </div>
-		 <div class="bottom-column">
+		</div>
+		<div class="bottom-column photo-actions">
 		 	<div class="contact-link">
 				<p>Cette photo vous intéresse ?</p>
         		<a href="#" class="contactButton" data-ref="<?php the_field('reference'); ?>">Contact</a>
@@ -42,9 +42,12 @@
       				<?php next_post_link('%link', '<img src="' . get_template_directory_uri() . '/assets/images/line7.png" alt="Photo suivante"', TRUE, ' ', 'categorie-photo'); ?>
     			</div>
 			</div>
-		 </div>
-	 </div>
-	 <?php
+		</div>
+		<div class="related-photos">
+		
+		</div>
+	</div>
+<?php
  endwhile; // End of the loop.
  
  get_footer();
