291f8a874cb7250d077ee3315b42becec9fb8882
Maxime Brunet <maximb84@gmail.com>
2023-09-14 10:52:27 +0200

    Mise en place JS emplissage champ réf modale

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
+/* Modale de contact, Header et page photographie avec remplissage réf */
 document.addEventListener("DOMContentLoaded", function() {
-  const contactButton = document.querySelector(".contactButton");
+  const contactButtons = document.querySelectorAll(".contactButton"); // Sélection de tous les boutons de contact
   const modal = document.querySelector("#contactModal");
+  const refField = modal.querySelector("#photo-reference"); 
 
-  // Initialiser les propriétés de la modale
+  // Initialiser propriétés modale mise en forme
   if (modal) {
     modal.style.display = 'none';
     modal.style.justifyContent = 'center';
     modal.style.alignItems = 'center';
   }
 
-  if (contactButton) {
-    contactButton.addEventListener("click", function() {
+  // Écouter événements du clic pour tous les boutons de contact
+  contactButtons.forEach(function(button) {
+    button.addEventListener("click", function(event) {
+      event.preventDefault();
+      const photoRef = this.getAttribute("data-ref"); // Récupération de la référence de la photo
+      
+      if (refField) {
+        refField.value = photoRef; // Remplissage automatique du champ
+      }
+
       if (modal) {
         if (modal.style.display === 'none') {
           modal.style.display = 'flex';
@@ -19,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
       }
     });
-  }
+  });
 
   // Pour fermer la modale en cliquant en dehors
   window.addEventListener("click", function(event) {
