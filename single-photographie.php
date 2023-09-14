e6ba81720f793aa486dfe32ac11952cb2ae566fb
Maxime Brunet <maximb84@gmail.com>
2023-09-14 10:21:50 +0200

    ajout de commentaires

--- single-photographie.php
+++ single-photographie.php
@@ -16,7 +16,7 @@ while ( have_posts() ) :
 	<div class="container">
 		<div class="both-columns">
 			<div class="left-column photo-info">
-				<h2><?php the_title(); ?></h2>
+				<h2><?php the_title(); ?></h2> <!-- récupération des éléments CPT -->
 				<p>Référence : <?php the_field('reference'); ?></p>
 				<p>Catégorie : <?php the_terms( $post->ID, 'categorie-photo', '', ', ', '' ); ?></p>
 				<p>Format : <?php the_terms( $post->ID, 'format-photo', '', ', ', '' ); ?></p>
@@ -24,19 +24,19 @@ while ( have_posts() ) :
 				<p>Date : <?php the_date('Y'); ?></p>
 			</div>
 			<div class="right-column photo-display">
-				<?php 
+				<?php                           // Vignette image de droite
 				if ( has_post_thumbnail() ) {
-				the_post_thumbnail('large');
+				the_post_thumbnail('full');
 				} 
 				?>
 			</div>
 		</div>
 		<div class="bottom-column photo-actions">
 		 	<div class="contact-link">
-				<p>Cette photo vous intéresse ?</p>
+				<p>Cette photo vous intéresse ?</p> <!-- bouton contact, A FINIR -->
         		<a href="#" class="contactButton" data-ref="<?php the_field('reference'); ?>">Contact</a>
     		</div>
-			<div class="navigation-links">
+			<div class="navigation-links"> <!-- Flèches navigations -->
 				<div class="previous">
       				<?php previous_post_link('%link', '<img src="' . get_template_directory_uri() . '/assets/images/line6.png" alt="Photo précédente">', TRUE, ' ', 'categorie-photo'); ?>
     			</div>
@@ -50,7 +50,7 @@ while ( have_posts() ) :
 			<h3>Vous aimerez aussi</h3>
 		</div>
 
-		<?php 
+		<?php // ARRAY requête wp_query pour les 2 photos images apparentées selon leur catégorie
 			$current_post_id = get_the_ID();
 			$categories = wp_get_post_terms( $current_post_id, 'categorie-photo', array( 'fields' => 'ids' ) );
 			$args = array(
@@ -78,8 +78,8 @@ while ( have_posts() ) :
 					
 					</div>
 					<?php endwhile; ?>
-					<?php wp_reset_postdata(); ?>
-					<?php else : ?>
+					<?php wp_reset_postdata(); ?> <!-- Si pas d'images supplémentaires -->
+					<?php else : ?> 
 					<p>Pas de photos apparentées.</p>
 				<?php endif; ?>
 			</div>
