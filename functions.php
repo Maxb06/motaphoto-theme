424d13dc3db5b269b71fdfb62f5ade964875e096
Maxime Brunet <maximb84@gmail.com>
2023-09-18 19:10:40 +0200

    Restauration fichier functions.php

--- functions.php
+++ functions.php
@@ -4,32 +4,23 @@ function motaphoto_enqueue_styles() {
     wp_enqueue_style( 'main-stylesheet', get_stylesheet_uri() );  
     wp_enqueue_style( 'custom-stylesheet', get_template_directory_uri() . '/css/styles.css' );
 }
-
 add_action( 'wp_enqueue_scripts', 'motaphoto_enqueue_styles' );
 
 
 function enqueue_my_scripts() {
   wp_enqueue_script( 'my-script', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0', true );
 }
-
 add_action( 'wp_enqueue_scripts', 'enqueue_my_scripts' );
 
 
+// Nav menu
 function register_my_menus() {
-    register_nav_menus(
-      array(
-        'header-menu' => __( 'Header Menu' ),
-        'footer-menu' => __( 'Footer Menu' ),
-        'extra-menu' => __( 'Extra Menu' )
-      )
-    );
-  }
-  add_action( 'init', 'register_my_menus' );
-
-
-/*
-add_action( 'after_setup_theme', 'your_theme_setup' );
-function your_theme_setup() {
-    add_image_size( 'custom-size', 563, 844, true );
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'footer-menu' => __( 'Footer Menu' ),
+      'extra-menu' => __( 'Extra Menu' )
+    )
+  );
 }
-*/
+add_action( 'init', 'register_my_menus' );
