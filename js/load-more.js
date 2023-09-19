6e986d7bd568d229ab9ee894f041ad82c63eb068
Maxime Brunet <maximb84@gmail.com>
2023-09-19 16:44:41 +0200

    Ajout code jquery requête ajax & API charger plus d'img

--- js/load-more.js
+++ js/load-more.js
@@ -1,1 +1,26 @@
+let page = 1; // initialisation de la variable page
 
+jQuery(document).ready(function($) {
+  $('#load-more-button').on('click', function() {
+    page++; // incrémenter le numéro de page
+    $.ajax({
+      url: `https://motaphoto.local/wp-json/wp/v2/photographie?page=${page}&per_page=12`, // adapter l'URL à votre API
+      method: 'GET',
+      success: function(data) {
+        data.forEach(function(post) {
+          let imageUrl = post.content.rendered.match(/src="([^"]+)"/)[1];
+          let html = `
+          <div class="photo-item">
+            <a href="${post.link}">
+              <img src="${imageUrl}" alt="${post.title.rendered}">
+            </a>
+          </div>`;
+          $('#photo-container').append(html);
+        });
+      },
+      error: function(err) {
+        console.log('Erreur :', err);
+      }
+    });
+  });
+});
