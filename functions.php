afbbec6d7082d15326e0652dda345fde84f81f2a
Maxime Brunet <maximb84@gmail.com>
2023-09-21 17:32:05 +0200

    Ajout fonction requete ajax

--- functions.php
+++ functions.php
@@ -26,3 +26,40 @@ function register_my_menus() {
   );
 }
 add_action( 'init', 'register_my_menus' );
+
+/* fonction pour traiter la requête Ajax */
+/*
+function load_more_posts() {
+    $paged = $_POST['page'] + 1;
+
+    $query = new WP_Query([
+        'post_type' => 'post',
+        'posts_per_page' => 8, // Nombre d'articles à charger (selon votre besoin)
+        'paged' => $paged
+    ]);
+
+    if ($query->have_posts()):
+        while ($query->have_posts()): $query->the_post();
+            get_template_part('template-parts/photo-block'); // Votre propre template pour chaque post
+        endwhile;
+    endif;
+
+    die();
+}
+
+add_action('wp_ajax_load_more', 'load_more_posts');
+add_action('wp_ajax_nopriv_load_more', 'load_more_posts');
+*/
+
+/* Enqueue Javascript */
+/*
+function enqueue_load_more_script() {
+  wp_enqueue_script('load-more', get_template_directory_uri() . '/js/load-more.js', ['jquery'], null, true);
+
+  wp_localize_script('load-more', 'load_more_params', [
+      'ajax_url' => admin_url('admin-ajax.php')
+  ]);
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_load_more_script');
+*/
