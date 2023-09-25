b95e00a5e5272c46dd433090442e83d909c7fa40
Maxime Brunet <maximb84@gmail.com>
2023-09-25 15:42:25 +0200

    Ajout structure code HTML lightbox

---
+++ template-parts/lightbox-modal.php
@@ -0,0 +1,12 @@
+<div id="lightbox-overlay" style="display:none;">
+    <div id="lightbox-container">
+        <img id="lightbox-image" src="" alt="Lightbox image" />
+        <div id="lightbox-info">
+            <p id="lightbox-title"></p>
+            <div id="lightbox-controls">
+                <button id="prev-button">Précédent</button>
+                <button id="next-button">Suivant</button>
+            </div>
+        </div>
+    </div>
+</div>
