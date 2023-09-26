e38b94e89a9c562d1d02ec976c7f9f923508890e
Maxime Brunet <maximb84@gmail.com>
2023-09-26 16:28:41 +0200

    Ajout images et mise à jour structure du code

--- template-parts/photo-block.php
+++ template-parts/photo-block.php
@@ -1,7 +1,14 @@
 <div class="photo-item">
-    <a href="<?php the_permalink(); ?>">
-        <?php if ( has_post_thumbnail() ) : ?>
+    <?php if ( has_post_thumbnail() ) : ?>
+        <a href="<?php the_permalink(); ?>">
             <?php the_post_thumbnail('full'); ?>
-        <?php endif; ?>
-    </a>
+        </a>
+        <div class="photo-overlay">
+            <a href="<?php the_permalink(); ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon_eye.png" alt="Voir la photo">
+            </a>
+            <img class="open-lightbox" data-image-url="<?php echo get_the_post_thumbnail_url(); ?>" src="<?php echo get_template_directory_uri(); ?>/assets/images/icon_fullscreen.png" alt="Agrandir la photo">
+        </div>
+    <?php endif; ?>
 </div>
+
