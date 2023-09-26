d8ec473c85d070d29310f756259c751c85582c5a
Maxime Brunet <maximb84@gmail.com>
2023-09-26 16:10:38 +0200

    Mise en place jQuery, corrections des classes et ID

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,4 +1,34 @@
 jQuery(document).ready(function($) {
-    
+    // Ouvrir la lightbox
+    $('.open-lightbox').on('click', function() {
+        var imageUrl = $(this).data('image-url');
+        $('#lightbox-image').attr('src', imageUrl);
+        //  ici, ajout titre ou d'autres info si nécessaire
+        $('#lightbox-overlay').show();
+    });
 
+    // Fermer la lightbox
+    $('#close-button').on('click', function() {
+        $('#lightbox-overlay').hide();
+    });
+
+    // Gérer les boutons précédent/suivant
+    $('#prev-button').on('click', function() {
+        var currentImage = $('#lightbox-image').attr('src');
+        var prevImage = $('.open-lightbox[data-image-url="' + currentImage + '"]').closest('.photo-item').prev('.photo-item').find('.open-lightbox').data('image-url');
+    
+        if (prevImage) {
+            $('#lightbox-image').attr('src', prevImage);
+        }
+    });
+    
+    $('#next-button').on('click', function() {
+        var currentImage = $('#lightbox-image').attr('src');
+        var nextImage = $('.open-lightbox[data-image-url="' + currentImage + '"]').closest('.photo-item').next('.photo-item').find('.open-lightbox').data('image-url');
+    
+        if (nextImage) {
+            $('#lightbox-image').attr('src', nextImage);
+        }
+    });
 });
+
