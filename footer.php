6720f736b0aa9ee3aecd722719e410fce3606581
Maxime Brunet <maximb84@gmail.com>
2023-10-05 17:01:51 +0200

    màj structure - Déplacement des 'php get_template_part'

--- footer.php
+++ footer.php
@@ -4,10 +4,11 @@
                 <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
                 <p>TOUS DROITS RÉSERVÉS</p>
             </div>
-            <?php get_template_part('template-parts/contact-modal'); ?>
-            <?php get_template_part('template-parts/lightbox-modal'); ?>
         </footer>
     </div> <!-- #page -->
+    <?php get_template_part('template-parts/contact-modal'); ?>
+    <?php get_template_part('template-parts/lightbox-modal'); ?>
+    
     <?php wp_footer(); ?>
 </body>
 </html>
